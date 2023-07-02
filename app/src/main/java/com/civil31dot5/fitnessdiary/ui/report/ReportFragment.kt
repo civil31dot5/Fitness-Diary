207ee67a29310da565541eaf611cc06de94e5669
jiazhang lin <civil31dot5@gmail.com>
2023-07-02 17:07:14 +0800

    [mod] 運動報告改用compose

--- app/src/main/java/com/civil31dot5/fitnessdiary/ui/report/ReportFragment.kt
+++ app/src/main/java/com/civil31dot5/fitnessdiary/ui/report/ReportFragment.kt
@@ -5,27 +5,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.civil31dot5.fitnessdiary.databinding.FragmentReportBinding
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.civil31dot5.fitnessdiary.domain.model.WeekReport
 import com.civil31dot5.fitnessdiary.ui.utility.FixedLabelXAxisRender
+import com.github.mikephil.charting.charts.CombinedChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.CombinedData
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 class ReportFragment : Fragment() {
-
-    private var _binding: FragmentReportBinding? = null
-    private val binding get() = _binding!!
 
     private val viewModel: ReportViewModel by viewModels()
 
@@ -33,19 +46,61 @@ class ReportFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentReportBinding.inflate(layoutInflater)
-        initView()
-        initListener()
-        return binding.root
+        return ComposeView(requireActivity()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ReportScreen(viewModel)
+            }
+        }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        bindViewModel()
+    @Composable
+    fun ReportScreen(viewModel: ReportViewModel) {
+
+        val chartData by viewModel.weekReport.collectAsStateWithLifecycle()
+
+        ReportContent(
+            chartData = chartData.sortedBy { it.yearWeek }
+        )
     }
 
-    private fun initView() {
-        binding.chartWeightCalories.apply {
+    @Composable
+    fun ReportContent(
+        chartData: List<WeekReport> = emptyList()
+    ) {
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "體重與運動卡路里 週變化圖",
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                factory = { context -> CombinedChart(context).apply { initWeightCaloriesChart(this) } },
+                update = { chart -> setWeightCaloriesChart(chart, chartData) }
+            )
+
+            Text(
+                text = "體脂率與運動卡路里 週變化圖",
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                factory = { context -> CombinedChart(context).apply { initFatRateCaloriesChart(this) } },
+                update = { chart -> setFatRateCaloriesChart(chart, chartData) }
+            )
+
+        }
+    }
+
+    private fun initWeightCaloriesChart(chart: CombinedChart) {
+        chart.apply {
             description = null
             axisRight.apply {
                 setDrawGridLines(false)
@@ -70,7 +125,10 @@ class ReportFragment : Fragment() {
             isHighlightPerDragEnabled = false
         }
 
-        binding.chartFatRateCalories.apply {
+    }
+
+    private fun initFatRateCaloriesChart(chart: CombinedChart) {
+        chart.apply {
             description = null
 
             axisRight.apply {
@@ -95,24 +153,9 @@ class ReportFragment : Fragment() {
             isHighlightPerTapEnabled = false
             isHighlightPerDragEnabled = false
         }
-
     }
 
-    private fun initListener() {
-
-    }
-
-    private fun bindViewModel() {
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.weekReport.collect {
-                    setupChart(it.sortedBy { it.yearWeek })
-                }
-            }
-        }
-    }
-
-    private fun setupChart(weekReposts: List<WeekReport>) {
+    private fun setWeightCaloriesChart(chart: CombinedChart, weekReposts: List<WeekReport>) {
 
         val xLabelMap = mutableMapOf<Float, WeekReport>()
 
@@ -129,11 +172,6 @@ class ReportFragment : Fragment() {
         val weightEntry = weekReposts.mapIndexedNotNull { index, weekReport ->
             weekReport.weekWeight ?: return@mapIndexedNotNull null
             Entry(index.toFloat(), weekReport.weekWeight!!.toFloat(), weekReport)
-        }
-
-        val fatRateEntry = weekReposts.mapIndexedNotNull { index, weekReport ->
-            weekReport.weekFatRate ?: return@mapIndexedNotNull null
-            Entry(index.toFloat(), weekReport.weekFatRate!!.toFloat(), weekReport)
         }
 
         val caloriesDataSetForWeight = BarDataSet(caloriesEntry, "運動卡路里").apply {
@@ -158,37 +196,15 @@ class ReportFragment : Fragment() {
             }
         }
 
-        val fatRateDataSet = LineDataSet(fatRateEntry, "體脂").apply {
-            axisDependency = YAxis.AxisDependency.LEFT
-            color = Color.BLUE
-            setDrawCircles(false)
-            setDrawCircleHole(false)
-            setDrawFilled(false)
-            valueFormatter = object : ValueFormatter() {
-                override fun getFormattedValue(value: Float): String {
-                    return "%.2f%%".format(value)
-                }
-            }
-        }
-
-
         val caloriesDataForWeight = BarData(caloriesDataSetForWeight)
-        val caloriesDataForFatRate = BarData(caloriesDataSetForWeight)
         val weightData = LineData(weightDataSet)
-        val fatRateData = LineData(fatRateDataSet)
-
 
         val weightCaloriesData = CombinedData().apply {
             setData(caloriesDataForWeight)
             setData(weightData)
         }
 
-        val fatRateCaloriesData = CombinedData().apply {
-            setData(caloriesDataForFatRate)
-            setData(fatRateData)
-        }
-
-        binding.chartWeightCalories.apply {
+        chart.apply {
             data = weightCaloriesData
 
             xAxis.valueFormatter = object : ValueFormatter() {
@@ -210,8 +226,58 @@ class ReportFragment : Fragment() {
             )
             invalidate()
         }
+    }
 
-        binding.chartFatRateCalories.apply {
+    private fun setFatRateCaloriesChart(chart: CombinedChart, weekReposts: List<WeekReport>) {
+
+        val xLabelMap = mutableMapOf<Float, WeekReport>()
+
+        weekReposts.forEachIndexed { index, weekReport ->
+            xLabelMap.put(index.toFloat(), weekReport)
+        }
+
+        val xLabelPosition = List(weekReposts.size) { index -> index.toFloat() }.toFloatArray()
+
+        val caloriesEntry = weekReposts.mapIndexed { index, weekReport ->
+            BarEntry(index.toFloat(), weekReport.weekSportCalories.toFloat(), weekReport)
+        }
+
+        val fatRateEntry = weekReposts.mapIndexedNotNull { index, weekReport ->
+            weekReport.weekFatRate ?: return@mapIndexedNotNull null
+            Entry(index.toFloat(), weekReport.weekFatRate!!.toFloat(), weekReport)
+        }
+
+        val caloriesDataSetForWeight = BarDataSet(caloriesEntry, "運動卡路里").apply {
+            axisDependency = YAxis.AxisDependency.RIGHT
+            valueFormatter = object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    return "%.0f".format(value)
+                }
+            }
+        }
+
+        val fatRateDataSet = LineDataSet(fatRateEntry, "體脂").apply {
+            axisDependency = YAxis.AxisDependency.LEFT
+            color = Color.BLUE
+            setDrawCircles(false)
+            setDrawCircleHole(false)
+            setDrawFilled(false)
+            valueFormatter = object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    return "%.2f%%".format(value)
+                }
+            }
+        }
+
+        val caloriesDataForFatRate = BarData(caloriesDataSetForWeight)
+        val fatRateData = LineData(fatRateDataSet)
+
+        val fatRateCaloriesData = CombinedData().apply {
+            setData(caloriesDataForFatRate)
+            setData(fatRateData)
+        }
+
+        chart.apply {
             data = fatRateCaloriesData
 
             xAxis.valueFormatter = object : ValueFormatter() {
@@ -232,11 +298,7 @@ class ReportFragment : Fragment() {
             )
             invalidate()
         }
-
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
 }
